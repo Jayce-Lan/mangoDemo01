a82067ccd1a3baf19fb8369ac42a2e3fcaccc829
jayce <jayce404@foxmail.com>
2021-02-26 09:56:38 +0800

    api集成swagger

---
+++ mango-api/src/main/java/com/mango/MangoApiApplication.java
@@ -0,0 +1,13 @@
+package com.mango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoApiApplication.class, args);
+	}
+
+}
